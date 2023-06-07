9486b2b62472fd281ab86b30046919c7d499587f
Nicolas Giese <nicolas.giese@i-sec.tuv.com>
2023-06-07 15:21:53 +0200

    Update firebasejs, add hint for Apple

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-messaging.js');
 
 const firebaseConfig = {
     apiKey: "AIzaSyCThkB9-GPLM45G9aFQfHm0oerXCFxJbR4",
